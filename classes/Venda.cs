6588ad0c520cf453a7a3dc731f644f3f8e59ee25
Gabriela Lima <gabicobra@gmail.com>
2017-10-27 19:22:42 -0200

    Criação do sumário da classe Venda

--- classes/Venda.cs
+++ classes/Venda.cs
@@ -1,13 +1,26 @@
 using System;
 
 namespace CadastroVendaPoo.classes{
+
+    /// <summary>
+    /// Esta classe fornece dados da venda
+    /// </summary>
     public class Venda{
         private string cpf;
         private int id;
         private DateTime datavenda;
-        
+    
+    /// <summary>
+    /// Esta classe constroi o objeto venda
+    /// </summary>
     public Venda() {}
 
+    /// <summary>
+    /// Esta classe constroi o objeto venda e é necessário imputar como parâmetros
+    /// </summary>
+    /// <param name="cpf">CPF como número</param>
+    /// <param name="id">Id  como número</param>
+    /// <param name="datavenda">data como data</param>
     public Venda(string cpf, int id, DateTime datavenda){
         this.cpf=cpf;
         this.id=id;
